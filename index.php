ea3458a01f6bb6d100ddcc23f0b5639d9135e853
Mélissa Kintz <kntzmelissa@gmail.com>
2021-03-15 16:47:55 +0100

    ajout title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>title</title>
+    <title>Le nouveau monde</title>
     <link rel="stylesheet" href="style.css" type="text/css">
   </head>
   <body>
@@ -17,7 +17,7 @@
       </section>
 
       <section id="carte">
-        <
+        
       </section>
 
       
