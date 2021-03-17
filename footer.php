0d5b88c93ba1b3a0e2b26d78c988224179475a65
Mélissa Kintz <kntzmelissa@gmail.com>
2021-03-17 14:53:31 +0100

    modif footer : add ista et fb

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
 <footer>
+
+
+    <a href="https://www.facebook.com/onepieceofficial"><img src="./assets/fblogo.png" width="40px"/></a>
+    <a href="https://www.instagram.com/onepiece_staff/?hl=fr"><img src="./assets/instalogo.png" width="40px"/></a>
 <p> Crédit : Selma, Antoine et Mélissa</p>
 </footer>
