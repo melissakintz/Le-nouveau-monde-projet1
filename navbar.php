6a0f93ba39b46f08aeb4e7cf3f0deeb6d4646e49
Antoine <Antoine.kuntz113@gmail.com>
2021-03-17 14:54:47 +0100

    on push

--- navbar.php
+++ navbar.php
@@ -1,11 +1,10 @@
 <header>
     <nav>
-         <ul>
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="Curiculum Vitae">Accueil</a></li>
-                <li><a href="Agenda.php">Disponibilités</a></li>
-                <li><a href="Tarifs.php">Tarifs</a></li>
-                <li><a href="contact.php">Contactez-nous</a></li>
-              </ul>
+        <ul>
+            <li><a href="./index.php">Accueil </a></li>
+            <li>Curriculum Vitae</li>
+            <li>Agenda</li>
+            <li>Contact</li>
+        </ul>
     </nav>
 </header>
