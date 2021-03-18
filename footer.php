0963a8eefef6bb5a8c11db7957dbf643299d7e68
Antoine <Antoine.kuntz113@gmail.com>
2021-03-18 15:12:17 +0100

    rectification banner

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <footer>
+<div id = "separation">
+    <hr style="height: 2px; color: #000000; background-color: #000000; width: 80%; border: none;"> 
+</div>
     <a href="https://www.facebook.com/onepieceofficial"><img src="./assets/fblogo.png" width="40px"/></a>
     <a href="https://www.instagram.com/onepiece_staff/?hl=fr"><img src="./assets/instalogo.png" width="40px"/></a>
-    <hr style="height: 2px; color: #000000; background-color: #000000; width: 50%; border: none;">
-    </section>
 <p> Crédit : Selma, Antoine et Mélissa</p>
 </footer>
