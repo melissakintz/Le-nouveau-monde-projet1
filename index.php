0ccc438aaf83110bd5f0d4f96c6546eccc24a263
Antoine <Antoine.kuntz113@gmail.com>
2021-03-19 19:13:33 +0100

    responsive  Background fini

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <body>
         <?php include "./navbar.php"?>
         <?php include "./header.php"?>
+
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/js/uikit.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/js/uikit-icons.min.js"></script>
     </body>
