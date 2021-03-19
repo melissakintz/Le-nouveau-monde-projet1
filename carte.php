44ca21b166de64e74978f35ee5b1e4d095fa091c
Mélissa Kintz <kntzmelissa@gmail.com>
2021-03-19 16:41:07 +0100

    FRAMEWORK navbar

---
+++ carte.php
@@ -0,0 +1,36 @@
+<div class="uk-child-width-1-2@m" uk-grid>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/franky1.jpg" alt="Franky">
+            </div>
+            <div class="uk-card-body">
+                <h3 class="uk-card-title">Franky</h3>
+                <p>Le meilleur charpentier </p>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="images/light.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <h3 class="uk-card-title">Media Top</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="images/light.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <h3 class="uk-card-title">Media Top</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+        </div>
+    </div>
+
+</div>
