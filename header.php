921952a4fc9d65c8e16c7718a46f2e73c4418991
Antoine <Antoine.kuntz113@gmail.com>
2021-03-19 18:03:00 +0100

    police ajouté

---
+++ header.php
@@ -0,0 +1,7 @@
+<header>
+<div>
+        <div class="uk-background-contain uk-background-muted uk-height-medium " style="background-image: url(./assets/banner31.jpg);">
+        <h1>Bienvenue dans le nouveau monde </h1>
+        </div>
+</div>
+</header>
