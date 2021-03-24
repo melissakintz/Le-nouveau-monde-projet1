d38db2c56a2a6bd375d831d7a92945987dc1d3c4
selmaBBK <selma.boubakri@hotmail.com>
2021-03-24 20:53:13 +0100

    modif

---
+++ FRheader.php
@@ -0,0 +1,24 @@
+<header>
+<div id="my-id">
+</div>   
+<h2 id = "Présentation"></h2>
+<div class="uk-child-width-1-2@s uk-light" uk-grid>
+    <div>
+        <div class="uk-background-top-right uk-background-cover uk-height-medium  uk-visible@s" style="background-image: url(./FRassets/franky1.gif);">
+    </div>  
+</div>
+<div>
+        <div class="uk-background-top-left uk-background-cover uk-height-medium uk-panel uk-flex uk-flex-middle uk-flex-center  uk-visible@s" style="background-image: url(./FRassets/franky3.gif);">
+        </div>
+</div>
+
+<script>
+    UIkit.util.on('#js-scroll-trigger', 'scrolled', function () {
+        alert('Done.');
+    });
+</script>
+</div>
+<section class = "Presentation">
+<h2> • Franky • Ingénieur Charpentier • 36 ans  </h2>
+</section>
+</header>
