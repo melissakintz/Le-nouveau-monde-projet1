353c3cad5d30b33b0022ec19acf479d1ec399a02
Mélissa Kintz <kntzmelissa@gmail.com>
2021-03-24 12:18:49 +0100

    structure page principale

--- tony.php
+++ tony.php
@@ -5,10 +5,61 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <<title>Le nouveau monde</title>
+    <<title>Curriculum Vitae | Tony Tony Chopper</title>
     <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/css/uikit.min.css" />
 </head>
 <body>
+    <?php include "#"?> <!--navbar-->
+    <?php include "#"?> <!--navbar-->
+    <main>
+        <section class = "Biographie">
+            <h2 id = "Biographie">• Biographie</h2>
+            <p>.............BIOGRAPHIE
+            </p>
+        </section>
+        <section class = "Compétence">
+            <h2 id = "Compétence">• Mes Compétences</h2>
+            <p>compétences  </p>
+        </section>
+        <section class = "imgCompétence">
+            <div class="uk-child-width-1-2@m uk-grid-match" uk-grid>
+                <div>
+                    <div class="uk-card uk-card-default uk-visible@s uk-card-body" uk-scrollspy="cls: uk-animation-slide-left; repeat: true">
+                        <img src="#">
+                    </div>
+                </div>
+                <div class="uk-card uk-card-default uk-visible@s uk-card-body" uk-scrollspy="cls: uk-animation-slide-right; repeat: true">
+                    <img src="#">
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default  uk-hidden@s uk-card-body" uk-scrollspy="cls: uk-animation-slide-left; repeat: true">
+                    <img src="#">
+                </div>
+            </div>
 
+        </section>
+
+        <section class = "Personnalité">
+            <h2 id = "Personnalité">• Ma Personnalité</h2>
+            <p>DESCRIPOTION PERSONNALITE</p>
+        </section>
+
+        <section class ="Contact uk-visible@s">
+            <img src="#"/>
+            <p> Aurais tu l'extrême amabilite de me montrer ta culotte !?</br>
+                AHHH non pas ma couronne !?#! </p>
+            <section id = "bateau">
+                <img src="#">
+            </section>
+        </section>
+
+    </main>
+
+    <?php include "./footer.php"?>
+
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/js/uikit-icons.min.js"></script>
 </body>
 </html>
