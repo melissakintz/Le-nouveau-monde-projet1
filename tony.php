2e295115539daad970df6b1317ee006de17c47cb
Mélissa Kintz <kntzmelissa@gmail.com>
2021-03-24 11:22:18 +0100

    structure

--- tony.php
+++ tony.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <title>Le nouveau monde</title>
-        <link rel="stylesheet" href="style.css" type="text/css">
-    </head>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <<title>Le nouveau monde</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
 
-    <body>
-
-    </body>
+</body>
 </html>
