9a634f4613e6f1eb45b5e7b0d73b5ca8dda70735
Antoine <Antoine.kuntz113@gmail.com>
2021-03-24 11:34:14 +0100

    supression main main2

--- AKbook.php
+++ AKbook.php
@@ -5,12 +5,67 @@
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <title>Le book de Brook</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/css/uikit.min.css" />
-        <link rel="stylesheet" href="AKstyle2.css" type="text/css">
+        <link rel="stylesheet" href="AKstyle.css" type="text/css">
     </head>
 
     <body>
     <?php include "./AKnavbar2.php"?>
-    <?php include "./AKmain2.php"?>  
+    <main>
+<section id = "bienvenue">
+<h2> Bienvenue dans le book de Brook , parcours avec moi les souvenirs présent sur cette pages :</h2>
+</section>
+<section id = "image">
+<div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
+    <div>
+        <div class="uk-card uk-card-hover uk-card-body">
+        <img src="./Akassets/1477069572_3229896985_1_4_FNrRqykY.gif "></br>
+      <p>Capitaine d'un convoi de bataille(anciennement), capitaine pirate (anciennement),</br> musicien escrimeur pirate</p>  
+    </div>
+    </div>
+
+    <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
+    <div>
+        <div class="uk-card uk-card-hover uk-card-body">
+            <img src="./Akassets/3138892950_1_12_NiDyI4Eu.gif ">
+        </div>
+    </div>
+
+    <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
+    <div>
+        <div class="uk-card uk-card-hover uk-card-body">
+        <img src="./Akassets/brook amoureux.gif ">
+        </div>
+    </div>
+</section>
+<section id = image2>
+<div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
+    <div>
+        <div class="uk-card uk-card-hover uk-card-body">
+        <img src="./Akassets/brook musique.gif ">
+    <p>Le Soul King Brook World Tour s'est </br>
+        achevé hier sur l'archiped Sabaody </br>
+    de facon aussi soudaine qu'incroyable </br>
+laissant les quelques 6xxx spectateurs venus acclamer la superstar </br>
+internationale de la Soul Music dans un mélange de désarroi, de colère et d'applaudissements.
+le célèbre musicien a quitté la scène pour reprendre ... son ancienne vie de pirate.</p>
+        </div>
+    </div>
+
+    <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
+    <div>
+        <div class="uk-card uk-card-hover uk-card-body">
+        <img src="./Akassets/f782bf9331d9e93411b7c1d267c1acad.gif ">
+        </div>
+    </div>
+
+    <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
+    <div>
+        <div class="uk-card uk-card-hover uk-card-body">
+        <img src="./Akassets/brook5.gif ">
+        </div>
+    </div> 
+</section>
+</main>
     <?php include "./AKfooter.php"?>   
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/js/uikit.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/js/uikit-icons.min.js"></script>
