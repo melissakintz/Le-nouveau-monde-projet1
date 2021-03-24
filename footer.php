7917e44df1ca1d24bd25ddfffada98e973a4927c
Antoine <Antoine.kuntz113@gmail.com>
2021-03-24 09:23:50 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,18 +1,21 @@
-<!--
+
 <footer>
-    <div id = "separation">
+<div id = "separation">
         <hr style="height: 2px; color: #000000; background-color: #000000; width: 80%; border: none;">
     </div>
 
-    <div id = "socialMedia">
-        <a href="https://www.facebook.com/onepieceofficial"><img src="./assets/fblogo.png" width="30vw"/></a>
-        <a href="https://www.instagram.com/onepiece_staff/?hl=fr"><img src="./assets/instalogo.png" width="40vw"/></a>
+    <section class = "crédit uk-visible@s">
+<p> Le nouveau monde – Tous Droits Réservés - U.S. No. de Brevet 7,599,930 B1; 7,827,125 and 7,836,060</p></br>
+<p>Le Site est édité par la société du nouveau monde, SAS au capital de 89.857.930 Berry</p></br>
+<p>dont le siège social est situé sur  l'île de Yukiryu. </p>
+    </section>
+    <div class = "Instagram">
+    <a href="https://www.instagram.com/onepiece_staff/?hl=fr"><img src="./AKassets/instalogo.png" width="40vw"/></a>
     </div>
-
-    <div id = "credit">
-        <p> Crédit : Selma, Antoine et Mélissa</p>
-    </div>
+    <div class = "Facebook">
+<a href="https://www.facebook.com/onepieceofficial/"><img src="./AKassets/facebooklogo.png" width="40vw"/></a>
+</div>
+</footer>
 
 </footer>
 
--->
