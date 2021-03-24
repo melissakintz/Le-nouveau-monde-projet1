d4a3fe9ffd42c87a1a70c794fc7c9aaced67ea91
Mélissa Kintz <kntzmelissa@gmail.com>
2021-03-24 11:30:48 +0100

    identation

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <header>
+    <div class="uk-background-contain uk-background-muted uk-height-medium uk-panel uk-flex uk-flex-middle  uk-flex-center uk-visible@s" style="background-image: url(./assets/banner31.jpg);">
+        <h1 style="font-size:2.5vw;">Bienvenue dans le nouveau monde </h1>
+        <h2 style="font-size:2vw;"> Recrutez la personne de vos rêves pour votre équipage.</h2>
+    </div>
 
-        <div class="uk-background-contain uk-background-muted uk-height-medium uk-panel uk-flex uk-flex-middle  uk-flex-center uk-visible@s" style="background-image: url(./assets/banner31.jpg);">
-        <h1 style="font-size:2.5vw;">Bienvenue dans le nouveau monde </br><h2 style="font-size:2vw;"> Recrutez la personne de vos rêves pour votre équipage.</h2></h1> 
-        </div> 
-        <div class="uk-background-contain uk-background-muted uk-height-small uk-panel uk-flex uk-flex-middle  uk-flex-center uk-hidden@s" style="background-image: url(./assets/ace.png);">
+    <div class="uk-background-contain uk-background-muted uk-height-small uk-panel uk-flex uk-flex-middle  uk-flex-center uk-hidden@s" style="background-image: url(./assets/ace.png);">
         <h1 style="font-size:4vw;">Bienvenue dans le nouveau monde </h1>
-        </div>
-        
+    </div>
 </header>
--- resultat.php
+++ resultat.php
@@ -1,21 +1,34 @@
-<link rel="stylesheet" href="style.css" type="text/css">
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/css/uikit.min.css" />
+</head>
+
+
+<body>
+
+
 
 <?php include "./navbar.php"?>
 
 <section id = "message">
-<p> <?php echo $_POST['equipage']?>, merci à toi d'avoir contacté le Nouveau monde , nous avons bien recu votre demande qui sera traité sous peu par l'un des membres de notre équipage.</br>
- Nous reviendrons vers vous avec une liste de candidats pouvant répondre a vos critères</p></br>
+    <p> <?php echo $_POST['equipage']?>, merci à toi d'avoir contacté le Nouveau monde , nous avons bien recu votre demande qui sera traité sous peu par l'un des membres de notre équipage.</br>
+    Nous reviendrons vers vous avec une liste de candidats pouvant répondre a vos critères</p></br>
  
- <p> <?php echo $_POST['equipage']?> a fait la demande suivante :</br>
-  Vous avez choisi les corps de métier suivant <?php echo $_POST['metiers']?>
-  avec un budget de <?php echo $_POST['budget']?> €</br>
- Nous vous recontacterons via l'adresse email <?php echo $_POST['email']?></br>
-  Merci de nous faire confiance pour votre demande, bonne fin de journée</p></br> 
+    <p> <?php echo $_POST['equipage']?> a fait la demande suivante :</br>
+    Vous avez choisi les corps de métier suivant <?php echo $_POST['metiers']?>
+    avec un budget de <?php echo $_POST['budget']?> €</br>
+    Nous vous recontacterons via l'adresse email <?php echo $_POST['email']?></br>
+    Merci de nous faire confiance pour votre demande, bonne fin de journée</p></br>
 </section>
 
 <?php include "./footer.php"?>
 
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/css/uikit.min.css" />
+
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/js/uikit.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.18/dist/js/uikit-icons.min.js"></script>
+
+</body>
+</html>
