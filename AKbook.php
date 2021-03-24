afba7b875cbe5cdfb22a5de3e9eaa57bcf75bc1b
Antoine <Antoine.kuntz113@gmail.com>
2021-03-24 14:59:26 +0100

    text fini pour de bon

--- AKbook.php
+++ AKbook.php
@@ -12,14 +12,14 @@
     <?php include "./AKnavbar2.php"?>
     <main>
 <section id = "bienvenue">
-<h2> Bienvenue dans le book de Brook , parcours avec moi les souvenirs présent sur cette pages :</h2>
+<h2> Bienvenue dans le book de Brook , parcours avec moi les souvenirs présent sur cette page :</h2>
 </section>
 <section id = "image">
 <div class="uk-child-width-1-2@s uk-grid-match" uk-grid>
     <div>
         <div class="uk-card uk-card-hover uk-card-body">
         <img src="./Akassets/1477069572_3229896985_1_4_FNrRqykY.gif "></br>
-      <p>Capitaine d'un convoi de bataille(anciennement), capitaine pirate (anciennement),</br> musicien escrimeur pirate</p>  
+      <p>Capitaine d'un convoi de bataille(anciennement)</br> capitaine pirate (anciennement)</br> musicien et  second épéiste d'un équipage pirate.</p>  
     </div>
     </div>
 
@@ -42,10 +42,10 @@
     <div>
         <div class="uk-card uk-card-hover uk-card-body">
         <img src="./Akassets/brook musique.gif ">
-    <p>Le Soul King Brook World Tour s'est </br>
+    <p>Le Soul King Brook World Tour s'est 
         achevé hier sur l'archiped Sabaody </br>
-    de facon aussi soudaine qu'incroyable </br>
-laissant les quelques 6xxx spectateurs venus acclamer la superstar </br>
+    de facon aussi soudaine qu'incroyable 
+laissant les quelques 6xxx spectateurs venus acclamer la superstar 
 internationale de la Soul Music dans un mélange de désarroi, de colère et d'applaudissements.
 le célèbre musicien a quitté la scène pour reprendre ... son ancienne vie de pirate.</p>
         </div>
